a2e83edf2cb40436207c77bc3e01a128ccfaa481
Adrian-ortiz0 <dxniel7328@gmail.com>
2026-01-03 12:13:28 -0500

    feat: :sparkles: avance 2

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.html',
+})
+export class GifsSideMenuOptions { 
+
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    }
+  ]
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: 'Tu app de gifs favorita',
+};
